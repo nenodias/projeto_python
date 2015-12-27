5ccb626711099f2ace5473be9136dee6d0e46add
Horácio Dias <horacio.dias92@gmail.com>
2015-12-27 18:39:04 -0200

    Brincando com o exemplo de acesso aos atributos e metodos getters e setter via setattr getattr e getattribute

--- copa_transparente/exemplos/exemplo_proxy.py
+++ copa_transparente/exemplos/exemplo_proxy.py
@@ -18,11 +18,29 @@ class DataTable:
         self._name = name
         self._data = data
 
+    def __setattr__(self, attr_name, value):
+        print('Acessando o atributo ',attr_name)
+        if hasattr(self, attr_name):
+            print('Atributo ',attr_name,' existe')
+            # O Return vai bloquear alterações
+            return 
+        super(DataTable, self).__setattr__(attr_name, value)
+
+    def __getattribute__(self, name):
+        print('Caindo no __getattribute__')
+        #Esse cara é mais baixo nível que o próprio getter e setter, que é usado no __getattr__ e setattr
+        return super(DataTable, self).__getattribute__(name)
+
     @property
     def name(self):
         return self._name
 
+    @name.setter
+    def name(self, value):
+        self._name = value
+
 tabela = DataTable(name='TabelaBacana', data=[3, 2, 1] )
 proxy = Proxy(tabela)
 print(proxy.__dict__)
+tabela.name = "LOL"
 print(proxy.name)
